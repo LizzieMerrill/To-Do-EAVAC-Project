09aff6014fa1af9a84756fdd4b1f7f41133bd47f
Elizabeth Merrill <eamerri2@asu.edu>
2022-08-08 19:05:33 -0700

    Created HTML skeleton and edited JS file (addItem function)

---
+++ script.js
@@ -0,0 +1,37 @@
+// list item storage structure
+// array of task objects
+
+class task {//done by Vyvian
+    constructor(name, dueDate, importance, isComplete) {
+        this.name = name;   //task name, string
+        this.dueDate = dueDate; //string 'yyyy-mm-dd'
+        this.importance = importance; //integer 1-3, 1 is highest importance
+        this.isComplete = isComplete; //boolean
+    }
+}
+
+//task list storage = array of tasks
+let myArray = [];
+
+
+
+
+//JavaScript to add new list item to HTML file
+//let mainList = document.querySelector('ol');
+
+
+
+function addItem(){
+let newLI = document.createElement('li');
+newLI.innerHTML = prompt("Enter a new to-do list item: ");
+newLI.setAttribute('id', )
+newLI.setAttribute('class', "thingToDo");
+newLI.ad
+//let mainList = document.querySelector('ol');
+//mainList.appendChild(newLI);
+
+}
+
+mainList.addEventListener('click', function removeItem(){
+        document.querySelector('.thingToDo').remove();
+    });
