59cd71794f249c20345e39cd57e07f21530808cc
acknice <acknicely@gmail.com>
2022-08-09 18:03:17 -0700

    object insert and table creation functions

--- script.js
+++ script.js
@@ -1,44 +1,67 @@
-// list item storage structure
-// array of task objects
-
-
-class task {//done by Vyvian
-
-
-    constructor(name, dueDate, priority, isComplete) {
+class Task {
+    constructor(name, dueDate, importance) { //dont pass isComplete
         this.name = name;   //task name, string
         this.dueDate = dueDate; //string 'yyyy-mm-dd'
-        this.priority = priority; //integer 1-3, 1 is highest importance
-        this.isComplete = isComplete; //boolean
+        this.importance = importance; //integer 1-3, 1 is highest importance
+        this.isComplete = false; //boolean
     }
 }
 
-
-//task list storage = array of tasks
 let myArray = [];
 
+function newObj(name_, dueDate_, importance_) {
+    if (name_ != null && dueDate_ != null && importance_ != null) {
+        myArray.push(new Task(name_,dueDate_,importance_))
+    }
+    else console.log("Input invalid") //text to html saying object not added bc missing input
+}
+
+
+function tableRefresh() {
+    document.querySelectorAll('td').remove();
+    document.querySelectorAll('tr.data-row').remove();
+    for (let i = 0; i < myArray.length; i++) {
+        let newRow = document.createElement('tr');
+        newRow.class = 'data-row';
+        document.querySelector('#task-table').appendChild(newRow);
+
+        let newName = document.createElement('td');
+        newName.innerHTML = myArray[i].name;
+        document.querySelector(newRow).appendChild(newName);
+
+        let newDate = document.createElement('td');
+        newDate.innerHTML = myArray[i].dueDate;
+        document.querySelector(newRow).appendChild(newDate);
+
+        let newPriority = document.createElement('td');
+        newPriority.innerHTML = myArray[i].importance;
+        document.querySelector(newRow).appendChild(newPriority);
+    }
+
+}
+
+function addTask() {
+    let addName = document.querySelector('#task-name').value;
+    let addDate = document.querySelector('#due-date').value;
+    let addPriority = document.querySelector('#priority').value;
+    
+    newObj(addName, addDate, addPriority);
+    tableRefresh();
+
+}
 
 
 
 //JavaScript to add new list item to HTML file
 //let mainList = document.querySelector('ol');
 
-
-
-function addItem(){
-let newLI = document.createElement('li');
-newLI.innerHTML = prompt("Enter a new to-do list item: ");
-newLI.setAttribute('id', )
-newLI.setAttribute('class', "thingToDo");
-newLI.ad
-//let mainList = document.querySelector('ol');
-//mainList.appendChild(newLI);
-
-}
+/*
 
 mainList.addEventListener('click', function removeItem(){
         document.querySelector('.thingToDo').remove();
     });
-=======
+*/
 //task list storage = array of tasks
->>>>>>> 32da99e3646d172651bf4f71cc054cfc18942d44
+
+
+
